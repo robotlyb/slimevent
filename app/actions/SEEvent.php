8dd34c19304b944a8d470ae1480e9bcf04fc68b9
Lijiahao <meitnerium07@gmail.com>
2012-09-26 09:58:29 +0800

    添加我的活动列表和创建列表和活动参与者

--- app/actions/SEEvent.php
+++ app/actions/SEEvent.php
@@ -2,7 +2,7 @@
 
 class SEEvent{
 
-   	function __construct()
+	function __construct()
 	{
 		if(Account::is_login() === FALSE)
 			F3::reroute('/');
@@ -13,49 +13,78 @@ class SEEvent{
 		//你需要修改下面的代码和models里Event.php里的createevent()函数,使得创建活动用户的id也存到event表里
 
 		$a =Event::createevent(
-			$_POST["title"],
-			$_POST["sort"],
-			$_POST["label"],
-			$_POST["location"],
-			$_POST["starttime"],
-			$_POST["endtime"],
-			$_POST["introduction"]
-		);
+				$id,
+				$_POST["title"],
+				$_POST["sort"],
+				$_POST["label"],
+				$_POST["location"],
+				$_POST["starttime"],
+				$_POST["endtime"],
+				$_POST["introduction"]
+				);
 
 		F3::reroute("/event/{$a}");
 	}
 
+	function show_join()
+	{
+		echo Template::serve('join.html');
+	}
 	function joins() 
 	{
 		$uid = Account::the_user_id(); //这个是当前登录用户的id
 		$eid = F3::get('PARAMS.eventID');  //这个是用户要参加的活动id
+		Event::joinevent($uid,$eid,$_POST["starttime"],$_POST["endtime"]);
 
-		/*想办法获取下面两个时间*/
-		//$spare_time_start =   //用户空闲的开始时间
-		//$spare_time_end =     //用户空闲的结束时间 
-
-		//之后你要做得就是把上现的参数传到models里的某个静态函数里,在那个静态函数里把这些参数存入数据库
-
-		echo "当前登录用户的id: ".$uid;
-		echo "用户要参加活动的id: ".$eid;
+		F3::reroute("/event/{$eid}");
 	}
+
 	function participants()
 	{
 		$eid = F3::get('PARAMS.eventID');  //这个是用户要参加的活动id
-		echo "参加活动的id: ".$eid;
+		//	echo "参加活动的id: ".$eid;
+	$event = Event::getevent($eid);
+	F3::set('event',$event[0]);
+		$result = Event::get_participant($eid);
+		$values = array();
+		foreach($result as $row)
+		{
+		
+			$values[]=array(
+					'name' => $row["name"],
+					'starttime' => $row["starttime"],
+					'endtime' => $row["endtime"]);
+		}
+		F3::set('values', $values);
+
+		echo Template::serve('participants.html');
+
 		//显示id为eid活动的所有参与者信息(名字,起始空闲时间)
 	}
 	function my()
 	{
 		$uid = Account::the_user_id(); //这个是当前登录用户的id
-		echo "当前登录用户的id: ".$uid;
-		
-		//根据uid从数据库搜索出该用户创建的所有活动以及参与的所有活动的信息
-		//之后在ui目录下创建一个html文件,用于显示当前用户创建活动以及参与活动的列表
+		//	echo "当前登录用户的id: ".$uid;
 
-		/*用于用户可能参与或创建了很多活动,不可能用一个个独立的变量把活动的信息传到html页面,所以你需要使用数组
-		最好的是,给html页面只传递两个数组,分别存储着创建的所有活动信息以及参与的所用活动信息
-		*/
+		$my_create = Event::my_create_event($uid);
+	
+         $values1=array();
+		foreach(F3::get('DB->result') as $row)
+		{
+		//	values1[]="<a href='/slimevent/event/{$row['event_id']}'>{$row["event_id"]}";
+    	$values1[]="<a href='/slimevent/event/{$row['event_id']}'>{$row["event_id"]}";
+		}
+        F3::set('values1',$values1);
+			$my_join = Event::my_join_event($uid);
+	     $values2=array();
+
+		foreach(F3::get('DB->result') as $row)
+		{
+		$values2[]="<a href='/slimevent/event/{$row['event_id']}'>{$row["event_id"]}";;
+		}
+        F3::set('values2',$values2);
+		echo Template::serve('my_event.html');
+
 	}
 
 	function show_create(){
--- app/models/Event.php
+++ app/models/Event.php
@@ -5,16 +5,38 @@ class Event{
 
 	static function getevent($id)
 	{
-    	return $r=DB::sql("select * from event where id= :id",array(':id' => $id));
+		return $r=DB::sql("select * from event where id= :id",array(':id' => $id));
 	}
 
-	static function createevent($title,$sort,$label,$location,$starttime,$endtime,$introduction)
+	static function createevent($id,$title,$sort,$label,$location,$starttime,$endtime,$introduction)
 	{
-	 	DB::sql("insert into event(title,sort,label,location,starttime,endtime,introduction) values(:title,:sort,:label,:location,:starttime,:endtime,:introduction)",array(':title' => $title,':sort' => $sort,':label' => $label,':location'=>$location,':starttime'=>$starttime,':endtime'=>$endtime,':introduction' => $introduction));
+		DB::sql("insert into event(title,sort,label,location,starttime,endtime,introduction) values(:title,:sort,:label,:location,:starttime,:endtime,:introduction)",array(':title' => $title,':sort' => $sort,':label' => $label,':location'=>$location,':starttime'=>$starttime,':endtime'=>$endtime,':introduction' => $introduction));
 
-		return DB::get_insert_id();
-
+		$event_id=DB::get_insert_id();
+		DB::sql("insert into user_createevent(user_id,event_id)values(:id,:event_id)",array(':id'=>$id,':event_id'=>$event_id));
+		return $event_id;
 	}
+	static function joinevent($user_id,$event_id,$starttime,$endtime)
+	{
+		echo $starttime;
+		echo $endtime;
+		DB::sql("insert into user_joinevent(user_id,event_id,starttime,endtime)values(:user_id,:event_id,:starttime,:endtime)",array(':user_id'=>$user_id,':event_id'=>$event_id,':starttime'=>$starttime,':endtime'=>$endtime));
+	}
+	static function get_participant($event_id)
+	{
+		return DB::sql("select name,starttime,endtime from snsUsers,user_joinevent where snsUsers.id = user_id and event_id = :event_id",array(':event_id' => $event_id)); 
+	}
+	static function my_create_event($user_id)
+	{
+		return DB::sql("select event_id from user_createevent where user_id = :user_id",array(':user_id' => $user_id));
+	}
+	static function my_join_event($user_id)
+	{
+
+		return DB::sql("select event_id from user_joinevent where user_id = :user_id",array(':user_id' => $user_id));
+	}
+
+
 };
 
 ?>
